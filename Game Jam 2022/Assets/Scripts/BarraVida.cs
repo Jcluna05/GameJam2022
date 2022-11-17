6b47e4ad9d561e8b7103f6c4de673787f2832e08
Edmartinez28 <davidmartinez2807@gmail.com>
2022-11-16 22:31:26 -0500

    Uno

---
+++ Game Jam 2022/Assets/Scripts/BarraVida.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarraVida : MonoBehaviour
+{
+    public Slider barra;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (barra.value <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
